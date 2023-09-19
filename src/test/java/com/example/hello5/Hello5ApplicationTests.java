9df98d24841d3a63ca31fd251ad2f86f6e645e83
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-09-19 22:28:04 +0900

    hello5の学習完了

---
+++ src/test/java/com/example/hello5/Hello5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
